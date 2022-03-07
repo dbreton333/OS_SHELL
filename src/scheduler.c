4375fd1524f5cef89636cba1824b62801745998f
David BRETON <dbreto7@teaching.CS.McGill.CA>
2022-03-07 18:03:33 -0500

    AGING final

--- src/scheduler.c
+++ src/scheduler.c
@@ -12,7 +12,9 @@ int AGING();
 int SJF();
 int badcommandNoSuchPolicy();
 void sortQueueL();
-struct PCB* decrement();
+void sortQueueS();
+void decrement();
+struct PCB* findSmaller();
 void switchPlace(struct PCB* pcb);
 
 int scheduler(char *policy){
@@ -40,6 +42,44 @@ void sortQueueL(){
 			struct PCB* prev = pcb->back;
 
 			if (prev != NULL && pcb->length > prev->length) {
+				pcb->back = prev->back;
+
+				if(prev->back != NULL){
+					prev->back->next = pcb;
+				}
+				prev->next = pcb->next;
+				if(pcb->next != NULL){
+					pcb->next->back = prev;
+				}
+				pcb->next = prev;
+				prev->back = pcb;
+
+				if(pcb == head){
+					head = prev;
+				}
+
+				if(prev == tail){
+					tail = pcb;
+				}
+				end = 0;
+			}
+
+			pcb = prev;
+		}
+	}
+}
+
+void sortQueueS(){
+	int end = 0;
+	struct PCB* pcb = head;
+
+	while(end != 1){
+		end = 1;
+
+		while (pcb != NULL){
+			struct PCB* prev = pcb->back;
+
+			if (prev != NULL && pcb->score > prev->score) {
 				pcb->back = prev->back;
 
 				if(prev->back != NULL){
@@ -189,130 +229,73 @@ int RR(){
 	return errCode;
 }
 
-struct PCB* decrement(){
+void decrement(){
 	struct PCB* pcb = head;
-	struct PCB* smaller = NULL;
+
 	while(pcb != NULL){
 		if(pcb != head){
 			if(pcb->score != 0){
 				pcb->score -= 1;
 			}
-			if(pcb->score < head->score){
-				if(smaller == NULL){
-					smaller = pcb;
-				}else{
-					if(pcb->score < smaller->score){
-						smaller = pcb;
-					}	
-				}
-			}
 		}
 		pcb = pcb->back;
-	}
-
-	return smaller;
-}
-
-void switchPlace(struct PCB* pcb) {
-	struct PCB* hd = head;
-
-	if(hd->back = pcb){
-		hd->back = pcb->back;
-
-		if(pcb->back != NULL){
-			pcb->back->next = hd;
-		}
-
-		pcb->next = NULL;
-
-		hd->next = pcb;
-		pcb->back = hd;
-
-	}else{
-		struct PCB* tmp1 = pcb->back;
-		struct PCB* tmp2 = pcb->next;
-
-		pcb->back = hd->back;
-		pcb->next = NULL;
-
-		hd->next = tmp2;
-		hd->back = tmp1;
-
 	}
 }
 
 
 int AGING(){
 	int errCode = 0;
-	int end = 0;
-
-	sortQueueL();
-
-	struct PCB* tmp = head;
+	sortQueueS();
 	
-	while (head != NULL){
-		
-		int index = tmp->PC;
-		for (int i = index; i < (tmp->length + tmp->base); i++){
-			char index[4];	
-			sprintf(index,"%d",i);
-			char* userInput = mem_get_value(index);
-			char* token;
-			char** liToken =  malloc(10 * sizeof(char*));;
-			int k = 0;
+	while(head != NULL){
 
-			//online mode -> checks if there is the symbole ;
-			if(strchr(userInput, ';') != NULL){
-		
-				token = strtok(userInput, ";");
+		char index[4];	
+		sprintf(index,"%d",head->PC);
+		char* userInput = mem_get_value(index);
+		char* token;
+		char** liToken =  malloc(10 * sizeof(char*));;
+		int k = 0;
 
-				while( token != NULL ) {
-					liToken[k] = malloc(200);
-					strcpy(liToken[k], token);
-					token = strtok(NULL, ";");
-					k++;
-				}
+		//online mode -> checks if there is the symbole ;
+		if(strchr(userInput, ';') != NULL){
+	
+			token = strtok(userInput, ";");
 
-				int j = 0;
-				
-				while( liToken[j] != NULL){
-					//parseInput for every instruction
-					errCode = parseInput(liToken[j]);
-					if (errCode == -1) exit(99);	// ignore all other errors
-					memset(liToken[j], 0, sizeof(liToken[j]));
-					free(liToken[j]); 
-					j++;
-				}
+			while( token != NULL ) {
+				liToken[k] = malloc(200);
+				strcpy(liToken[k], token);
+				token = strtok(NULL, ";");
+				k++;
 			}
-			else{
 
-				errCode = parseInput(userInput);
+			int j = 0;
+			
+			while( liToken[j] != NULL){
+				//parseInput for every instruction
+				errCode = parseInput(liToken[j]);
 				if (errCode == -1) exit(99);	// ignore all other errors
-				memset(userInput, 0, sizeof(userInput));	
+				memset(liToken[j], 0, sizeof(liToken[j]));
+				free(liToken[j]); 
+				j++;
 			}
-
-			free(liToken);
-
-			tmp->PC += 1;
-
-			struct PCB* pcbs = decrement();
-
-			if(pcbs != NULL){
-				switchPlace(pcbs);
-				break;
-			}
-
 		}
+		else{
 
-		//clear pcb when pcb reaches the end
-		if(tmp->PC == (tmp->length + tmp->base)){
-			PCB_clear(tmp);
+			errCode = parseInput(userInput);
+			if (errCode == -1) exit(99);	// ignore all other errors
+			memset(userInput, 0, sizeof(userInput));	
 		}
+		
+		free(liToken);
+		head->PC += 1;
 
-		tmp = head;
+		if(head->PC == (head->length + head->base)){
+			PCB_clear(head);
+		}
+		decrement();
+		sortQueueS();
 
 	}
-
 	return errCode;
 }
 
